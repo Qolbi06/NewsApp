1618a6f41ad80222e6285916353954db7541d906
Luthfi Qolbi <luthfiqolbi@gmail.com>
2024-03-21 14:29:22 +0700

    update

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            // cek credentials (login)
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt([
+                'email' => $credentials['email'],
+                'password' => $credentials['password']
+            ])) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            };
+
+            // cek klo password g sesuai
+            $user = User::where('email', $credentials['email'])->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            };
+
+            // jika berasil cek password maka loginkan
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'bearer',
+                'user' => $user
+            ], 'Authenticated', 200);
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+    
+    public function register(Request $request)
+    {
+        try {
+            $this->validate($request, [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|string|min:6',
+                'confirm_password' => 'required|string|min:6'
+            ]);
+
+            // cek kondisi password dan confirm password
+            if ($request->password != $request->confirm_password) {
+                return ResponseFormatter::error([
+                    'message' => 'Password not match'
+                ], 'Authentication Failed', 401);
+            }
+
+            // create akun
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            // get data akun
+            $user = User::where('email', $request->email)->first();
+
+            // create token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            // Response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Authenticated', 200);
+
+        } catch (\Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something wnt wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function logout(Request $request){
+        $token = $request->user()->currentAccessToken()->delete();
+        return ResponseFormatter::success(
+            [
+                $token, 'Token Revoked', 200
+        ]
+        );
+    }
+}
